9abeab38bc39741c062288f21cf107ecf66c4312
Swimaster01 <52829444+Swimaster01@users.noreply.github.com>
2026-09-17 13:17:06 +0600

    Completed the project

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-const button = document.querySelector('.toggle-button');
+const buttons = document.querySelectorAll('.toggle-button');
+const answer = document.querySelector('.q1-answer');
+const icon = document.querySelector('.toggle-button img');
+const plusIcon = '/faq-accordion-main/assets/images/icon-plus.svg';
+const minusIcon = '/faq-accordion-main/assets/images/icon-minus.svg';
 
-button.addEventListener('click', function(){
-    console.log('Button clicked!');
 
+buttons.forEach(function(button) {
+
+    const questionContainer = button.closest('.question-wrapper');
+    const answer = questionContainer.querySelector('.answer'); 
+    const icon = button.querySelector('img');
+
+    let isOpen = false; // Track the state of the answer
+
+    button.addEventListener('click', function() {
+
+        isOpen = !isOpen; // Toggle the state
+    
+        if (isOpen) {
+            answer.classList.add('open');
+            icon.src = minusIcon;
+        }
+        else {
+            answer.classList.remove('open');
+            icon.src = plusIcon;
+        }
+
+        
+    });
 });
+
+
+
+
